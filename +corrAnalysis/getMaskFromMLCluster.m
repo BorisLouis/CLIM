0e015dafee03702c6736c5858fbe75e1f30901bd
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2020-11-20 11:56:23 +0100

    Delete function as too small

--- +corrAnalysis/getMaskFromMLCluster.m
+++
@@ -1,9 +0,0 @@
-function [MLCorrMask] = getMaskFromMLCluster(clust,inds,clust2Use,dim)
-    
-    MLCorrMask = zeros(dim);
-
-    for i = 1:length(inds)
-        MLCorrMask(inds(i)) = clust(i,clust2Use);
-    end
-    
-end
