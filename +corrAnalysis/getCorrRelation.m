399ad20ab9eff5074b4d2c26ea4663106603b328
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2021-06-23 16:28:22 +0200

    Threshold plotting and testing

--- +corrAnalysis/getCorrRelation.m
+++ +corrAnalysis/getCorrRelation.m
@@ -1,8 +1,9 @@
-function [listCorrPx,sumCorrPx] = getCorrRelation(data2Cluster,r,corrThreshold)
+function [listCorrPx,sumCorrPx] = getCorrRelation(data2Cluster,r,startThreshold)
     %function to find correlation relation between a each pixel of
     %an image and its neighbor pixels
     
     corrRel  = cell(size(data2Cluster,1),size(data2Cluster,2));
+    corrVal  = cell(size(data2Cluster,1),size(data2Cluster,2));
     %loop through pixels
     for i = 1:size(data2Cluster,1)
         for j = 1:size(data2Cluster,2)
@@ -29,19 +30,58 @@ function [listCorrPx,sumCorrPx] = getCorrRelation(data2Cluster,r,corrThreshold)
             corr(neighborIdx==currPxIdx) = [];
             neighborIdx(neighborIdx==currPxIdx) = [];
 
-            if all(corr>corrThreshold)
-
-            else
-                idx = neighborIdx(corr<corrThreshold,:);
-
-                if ~isempty(idx)
-                   %convert to indices for simplicity later
-                   corrRel{currPxIdx} = idx;
-                end
-            end
+%             if all(corr>corrThreshold)
+% 
+%             else
+%                 idx = neighborIdx(corr<corrThreshold,:);
+% 
+%                 if ~isempty(idx)
+%                    %convert to indices for simplicity later
+%                    corrRel{currPxIdx} = idx;
+%                 end
+%             end
+            corrRel{currPxIdx} = neighborIdx;
+            corrVal{currPxIdx} = corr;
         end
     end
     
+    %here we test the thresholds
+    disp('stop');
+    disp('Searching for optimal threshold');
+    totArea = size(corrRel,1)*size(corrRel,2);
+    area = zeros(1,30);
+    meanCorr = zeros(1,30);
+    threshold = zeros(1,30);
+    for i = 1:50
+       
+       
+       %find cell with value bigger than threshold
+       idx = cellfun(@(x) x<startThreshold,corrVal,'UniformOutput',false);
+       test = cellfun(@(x) sum(x)>0,idx);
+       area(i) = sum(test(:));
+       
+       currentData = corrVal(test);
+       
+       meanCorr(i) = mean(cellfun(@(x) mean(x(x<startThreshold)),currentData));
+       
+       threshold(i) = startThreshold;
+       startThreshold = startThreshold - 0.02;
+       
+    end
+    
+    while(true)
+       %Test threhsold
+       
+        
+        
+        
+    end
+    
+    
+    
+    
+    
+    %reshape
     listCorrPx = reshape(corrRel,size(corrRel,1)*size(corrRel,2),1);
     sumCorrPx  = cellfun(@sum,listCorrPx);
 end
